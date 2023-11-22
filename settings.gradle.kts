8cb07b54e94359475a68341befb170d69ac3d0bf
klu2 <172195+klu2@users.noreply.github.com>
2023-11-22 16:25:34 +0100

    cleanup file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,4 @@
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-plugins {
-}
-
-include(":test-server")
 include(":test-api")
+include(":test-server")
 
-
-rootProject.name = "untitled"
+rootProject.name = "swagger-codegen-test"
